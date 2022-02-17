e2995d33c06b9742dcd9ed6d559250c4b4f79072
aimunhidden485 <aimunnaharr@gmail.com>
2022-02-17 09:11:41 +0600

    balance error handled

--- js/money.js
+++ js/money.js
@@ -7,13 +7,24 @@ const clothesInput = document.getElementById('clothes-input')
 const clothesAmount = parseFloat(clothesInput.value)
 const totalExpense = foodAmount+ rentAmount + clothesAmount
 const totalExpenseAmount = document.getElementById('total-cost')
-totalExpenseAmount.innerText = parseFloat(totalExpense).toFixed(2)
+// totalExpenseAmount.innerText = parseFloat(totalExpense).toFixed(2)
 const incomeInput = document. getElementById('income-input')
 const incomeAmount = parseFloat(incomeInput.value)
 const totalBalance = incomeAmount - totalExpense
 const balanceAmount = document.getElementById('balance-amount')
-balanceAmount.innerText = totalBalance
-if(isNaN(foodInput.value)){
+// balanceAmount.innerText = totalBalance
+if(foodInput.value< 0 || rentInput.value < 0 || clothesInput.value< 0 || incomeInput.value<0){
     
+    document.getElementById('balance-error').style.display = 'block'
+}
+else if(isNaN(foodInput.value) || isNaN(rentInput.value) || isNaN(clothesInput.value) || isNaN(incomeInput.value)){
+    document.getElementById('balance-error').style.display = 'block'
+}
+else if(incomeAmount<totalExpense){
+   document.getElementById('income-error').style.display = 'block'
+}
+else {
+    balanceAmount.innerText = totalBalance
+    totalExpenseAmount.innerText = parseFloat(totalExpense).toFixed(2)
 }
 })
