518f6ab982ee5ac542b81db2806cb14d03bd2aa2
aimunhidden485 <aimunnaharr@gmail.com>
2022-02-17 08:11:06 +0600

    calculate button added

--- js/money.js
+++ js/money.js
@@ -1,3 +1,19 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
+const foodInput = document.getElementById('food-input')
+const foodAmount = parseFloat(foodInput.value)
+const rentInput = document.getElementById('rent-input')
+const rentAmount = parseFloat(rentInput.value)
+const clothesInput = document.getElementById('clothes-input')
+const clothesAmount = parseFloat(clothesInput.value)
+const totalExpense = foodAmount+ rentAmount + clothesAmount
+const totalExpenseAmount = document.getElementById('total-cost')
+totalExpenseAmount.innerText = parseFloat(totalExpense).toFixed(2)
+const incomeInput = document. getElementById('income-input')
+const incomeAmount = parseFloat(incomeInput.value)
+const totalBalance = incomeAmount - totalExpense
+const balanceAmount = document.getElementById('balance-amount')
+balanceAmount.innerText = totalBalance
+if(isNaN(foodInput.value)){
     
+}
 })
